5a4ac3e35d55fc722fe4878a80a31a130249b19f
Matthew Berryman <matthew@acrossthecloud.net>
2018-06-01 22:03:36 +1000

    some fixes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,7 +7,7 @@ export const Header: React.StatelessComponent<{}> = () => {
       <nav className="navbar navbar-default">
         <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
           <ul className="nav navbar-nav">
-            <li><Link to="/entry">About</Link></li>
+            <li><Link to="/entry">Entry Form</Link></li>
             <li><Link to="/">Home</Link></li>
           </ul>
         </div>
