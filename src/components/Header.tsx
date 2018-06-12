62dea735a76049099ab3d4936ca8087623e34d03
Matthew Berryman <matthew@acrossthecloud.net>
2018-06-12 11:07:18 +1000

    fixes #3 bootstrap menu (on small page width) not working

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,4 @@
 import * as React from 'react';
-
-import { NavProps } from 'reactstrap';
-
 import {
   Collapse,
   Navbar,
@@ -11,46 +8,42 @@ import {
   NavItem,
   NavLink } from 'reactstrap';
 
-export default class Header extends React.Component<NavProps, {dropdownOpen: Boolean}> {
+export default class Header extends React.Component<{}, {isOpen: boolean}> {
 
-    state: { dropdownOpen: boolean; };
+  state: {isOpen: false};
 
-    setState(arg0: {dropdownOpen: boolean}): void {
-      this.state = arg0;
-    }
+  constructor(props: any) { // tslint:disable-line: no-any
+    super(props);
 
-    constructor(props: NavProps) {
-      super(props);
-
-      this.toggle = this.toggle.bind(this);
-      this.state = {
-        dropdownOpen: false
-      };
-    }
-    toggle() {
-      this.setState({
-        dropdownOpen: !this.state.dropdownOpen
-      });
-    }
-
-    render() {
-      return (
-        <div>
-          <Navbar color="light" light expand="md">
-            <NavbarBrand href="/">TBA21 OceanArchive</NavbarBrand>
-            <NavbarToggler onClick={this.toggle} />
-            <Collapse dropdownOpen={this.state.dropdownOpen} navbar>
-              <Nav className="ml-auto" navbar>
-                <NavItem>
-                  <NavLink href="/">Home</NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink href="/entry">Metadata Entry</NavLink>
-                </NavItem>
-              </Nav>
-            </Collapse>
-          </Navbar>
-        </div>
-      );
-    }
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
   }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+  render() {
+    return (
+      <div>
+        <Navbar color="light" light expand="md">
+          <NavbarBrand href="/">TBA21</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href="/home">Home</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/entry">Metadata Entry</NavLink>
+              </NavItem>
+
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
