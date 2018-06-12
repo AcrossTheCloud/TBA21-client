d8aebdfe2754ee697bd197f502283fd9e7bea69c
Matthew Berryman <matthew@acrossthecloud.net>
2018-06-12 11:08:11 +1000

    fixes #4 form submission not working

--- src/components/EntryForm.tsx
+++ src/components/EntryForm.tsx
@@ -1,8 +1,67 @@
 import * as React from 'react';
 import { observer } from 'mobx-react';
 import { FormState, FieldState } from 'formstate';
-
 import { Form, FormGroup, Label, Input, Button, Container } from 'reactstrap';
+import {
+    Viewport,
+    InteractiveMap,
+    MapEvent
+} from 'react-map-gl';
+
+import * as MapboxGL from 'mapbox-gl';
+
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+interface MyMapState {
+  viewport: Viewport;
+}
+
+interface MyMapProps {
+  onClick: (e: MapEvent) => void; // tslint:disable-line: no-any
+}
+
+class MyMap extends React.Component<MyMapProps, MyMapState> {
+
+  map: MapboxGL.Map;
+
+  state: MyMapState = {
+      viewport: {
+          bearing: 0,
+          isDragging: false,
+          longitude: 150.86914,
+          latitude: -34.41921,
+          zoom: 3,
+      }
+  };
+
+  _onViewportChange = (viewport: Viewport) => this.setState({viewport});
+
+    constructor (props: any) { // tslint:disable-line: no-any
+      super(props);
+      this.props = props;
+    }
+
+    render() {
+        return (
+            <div>
+                <InteractiveMap
+                    {...this.state.viewport}
+                    mapboxApiAccessToken="pk.eyJ1IjoiYWNyb3NzdGhlY2xvdWQiLCJhIjoiY2ppNnQzNG9nMDRiMDNscDh6Zm1mb3dzNyJ9.nFFwx_YtN04_zs-8uvZKZQ"
+                    height={400}
+                    width={400}
+                    ref={this.setRefInteractive}
+                    onViewportChange={this._onViewportChange}
+                    onClick={this.props.onClick}
+                />
+            </div>
+        );
+    }
+
+    private readonly setRefInteractive = (el: InteractiveMap) => {
+        this.map = el.getMap();
+    }
+
+}
 
 let regexWeburl = new RegExp(
   '^' +
@@ -44,42 +103,82 @@ let regexWeburl = new RegExp(
 
 const oceans: String[] = [ 'Pacific', 'Atlantic', 'Indian', 'Southern', 'Arctic' ];
 
+const valposition = function (lngLat: number[]): boolean {
+  let lng = lngLat[0], lat = lngLat[1];
+  return (-180 <= lng) && (lng <= 180) && (-90 <= lat) && (lat <= 90);
+};
+
 class EntryFormState {
   // Create a field
-  description = new FieldState('').validators((val) => !val && 'description required');
-  ocean = new FieldState('Pacific').validators((val) => oceans.indexOf(val) < 0 && 'valid ocean requured');
-  url = new FieldState('').validators((val) => regexWeburl.test(val) && 'valid URL required');
-  artist = new FieldState('').validators((val) => { return false; });
+  description = new FieldState('').validators((val: string) => !val && 'description required');
+  ocean = new FieldState('Pacific').validators((val: string) => oceans.indexOf(val) < 0 && 'valid ocean requured');
+  url = new FieldState('').validators((val: string) => !regexWeburl.test(val) && 'valid URL required');
+  artist = new FieldState('').validators((val: string) => { return false; });
+  position = new FieldState([150.86914, -34.41921]).validators((val: number[]) => !valposition(val) && 'valid position required');
 
   // Compose fields into a form
   form = new FormState({
     description: this.description,
     ocean: this.ocean,
     url: this.url,
+    position: this.position,
     artist: this.artist
   });
 
-  onSubmit = async () => {
+  onSubmit = async (e: any) => { // tslint:disable-line:no-any
+    e.preventDefault(); 
     //  Validate all fields
-    const res = await this.form.validate();
+    let res = await this.form.validate();
     // If any errors you would know
     if (res.hasError) {
-      // fixme
       return;
     }
-  // Yay .. all good. Do what you want with it
-  // fixme
+    // Yay .. all good. Do what you want with it
+    let body = JSON.stringify({
+      description: this.form.$.description.$,
+      ocean: this.form.$.ocean.$,
+      url: this.form.$.url.$,
+      position: this.form.$.position.$,
+      artist: this.form.$.artist.$
+    });
+
+    try {
+      let response = await fetch('https://4984s3d656.execute-api.eu-central-1.amazonaws.com/prod/item', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: body
+      });
+      await response.status;
+      location.reload();
+    } catch (err) {
+      alert(err);
+    }
   }
+
 }
 
 @observer
 export class EntryForm extends React.Component<{}, {}> {
+
   data = new EntryFormState();
+
+  setposition = (e: MapEvent) => {
+    this.data.position.onChange(e.lngLat);
+  }
+
   render() {
     const data = this.data;
     return (
       <Container>
-      <Form onSubmit={data.onSubmit}>
+      <MyMap onClick={(e) => this.setposition(e)}/>
+      <Form onSubmit={(e) => data.onSubmit(e)}>
+        <FormGroup>
+          <Label for="position">position</Label>
+          <Input
+            value={data.position.value.toString()}
+            readOnly={true}
+          />
+        </FormGroup>
         <FormGroup>
           <Label for="inputDescription">Description</Label>
           <Input
