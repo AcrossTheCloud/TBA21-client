4d08fcbce2b597234d374bfb2a04a340f472e403
Mehrdad Amirghasemi <mehrdad.amirghasemi@gmail.com>
2018-12-16 17:51:53 +1100

    attempt to fix #23

--- src/components/NetworkGraph.tsx
+++ src/components/NetworkGraph.tsx
@@ -28,7 +28,7 @@ export class NetworkGraph extends React.Component<{}, Graph> {
         <div>
         {this.state.nodes.length > 0 ? (
           <Sigma
-            renderer="canvas"
+            renderer="webgl"
             style={{maxWidth: 'inherit', height: '400px'}}
             settings={{
               labelThreshold: 0,
