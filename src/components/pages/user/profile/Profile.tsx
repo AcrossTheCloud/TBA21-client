6626f4d93991fd30c772c313ade4939a4e5cd4f7
Dan Wood <daniel@acrossthecloud.net>
2019-04-18 12:51:56 +1000

    OC-18 Moved ChangePassword out of Profile and into it's on Util Also moved DeletePassword into it's own util.

--- src/components/pages/user/profile/Profile.tsx
+++ src/components/pages/user/profile/Profile.tsx
@@ -4,7 +4,8 @@ import { Alert, Button, Container, Form, FormGroup, Input, Label } from 'reactst
 import { get, has } from 'lodash';
 import * as $ from 'jquery';
 
-import DeleteAccount from './DeleteAccount';
+import DeleteAccount from '../../../utils/user/DeleteAccount';
+import ChangePassword from '../../../utils/user/ChangePassword';
 import { getCurrentAuthenticatedUser, logout } from '../../../utils/Auth';
 import { deleteAccount, dispatchError, updateAttributes, changePassword } from 'src/actions/user/profile';
 
@@ -27,7 +28,6 @@ interface Props {
 interface State {
   isAuthenticated: boolean;
   credentials: {};
-  passwordsMatch: boolean;
 }
 
 const ErrorMessage = (props: {message: string | boolean}) => {
@@ -51,21 +51,14 @@ const SuccessMessage = (props: {message: string | boolean}) => {
 
 class Profile extends React.Component<Props, State> {
   private emailInput;
-  private newPasswordInput;
-  private oldPasswordInput;
-  private confirmPasswordInput;
-  private confirmPasswordOnChangeTimeout;
 
   constructor(props: Props) {
     super(props);
 
     this.state = {
       isAuthenticated: false,
-      credentials: {},
-      passwordsMatch: false
+      credentials: {}
     };
-
-    this.confirmPasswordOnChangeTimeout = false;
   }
 
   async componentDidMount(): Promise<void> {
@@ -114,36 +107,6 @@ class Profile extends React.Component<Props, State> {
     }
   }
 
-  changePassword = () => {
-    this.props.changePassword(this.oldPasswordInput.value, this.newPasswordInput.value);
-    this.oldPasswordInput.value = '';
-    this.newPasswordInput.value = '';
-    this.confirmPasswordInput.value = '';
-    this.setState({ passwordsMatch: false });
-  }
-
-  confirmPasswordOnChange = () => {
-    const cancelTimeout = () => {
-      clearTimeout(this.confirmPasswordOnChangeTimeout);
-      this.confirmPasswordOnChangeTimeout = false;
-    };
-
-    if (!this.confirmPasswordOnChangeTimeout && this.oldPasswordInput.value) {
-      this.confirmPasswordOnChangeTimeout = setTimeout( () => {
-        if (this.newPasswordInput.value === this.confirmPasswordInput.value) {
-          cancelTimeout();
-          this.setState({ passwordsMatch: true });
-        } else {
-          cancelTimeout();
-          this.setState({ passwordsMatch: false });
-        }
-      },
-                                                        500);
-    } else {
-      cancelTimeout();
-    }
-  }
-
   render() {
     const credentials = this.state.credentials;
 
@@ -176,23 +139,7 @@ class Profile extends React.Component<Props, State> {
           <Button>Submit</Button>
         </Form>
 
-        <h3>Change Password</h3>
-        <Form onSubmit={(e) => { e.preventDefault(); this.changePassword(); }} autoComplete="off">
-          <FormGroup>
-            <Label for="oldPassword">Old Password</Label>
-            <Input type="password" name="oldPassword" id="oldPassword" placeholder="Old Password" innerRef={e => this.oldPasswordInput = e}/>
-          </FormGroup>
-
-          <FormGroup>
-            <Label for="newPassword">New Password</Label>
-            <Input type="password" name="newPassword" id="newPassword" placeholder="New Password" innerRef={e => this.newPasswordInput = e}/>
-
-            <Label for="confirmPassword">Confirm Password</Label>
-            <Input type="password" name="confirmPassword" id="confirmPassword" onChange={this.confirmPasswordOnChange} placeholder="Confirm Password" innerRef={e => this.confirmPasswordInput = e}/>
-          </FormGroup>
-
-          {this.state.passwordsMatch ? <Button>Change</Button> : <></>}
-        </Form>
+        <ChangePassword changePassword={this.props.changePassword} />
 
         <DeleteAccount isAuthenticated={this.state.isAuthenticated} deleteAccountAction={this.props.deleteAccount}/>
       </Container>
