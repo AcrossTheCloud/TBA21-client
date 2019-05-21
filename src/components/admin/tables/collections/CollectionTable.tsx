390961231a62dcf270ddff7d5d4753b95c01e72f
Matthew Berryman <matthew@acrossthecloud.net>
2019-05-21 13:15:20 +1000

    fix import of Position

--- src/components/admin/tables/collections/CollectionTable.tsx
+++ src/components/admin/tables/collections/CollectionTable.tsx
@@ -3,7 +3,8 @@ import BootstrapTable from 'react-bootstrap-table-next';
 import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
 import { Button, Modal, ModalBody, ModalFooter, Spinner } from 'reactstrap';
 
-import DraggableMap, { Position } from 'components/map/DraggableMap';
+import DraggableMap from 'components/map/DraggableMap';
+import { Position } from 'types/Map';
 import Tags, { Tag } from '../Tags';
 
 import 'styles/components/admin/tables/modal.scss';
