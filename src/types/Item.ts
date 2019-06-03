e8a3929bc6bf33272757bd90406fe7b04bbb49b3
Dan Wood <daniel@acrossthecloud.net>
2019-06-03 12:26:46 +1000

    Change in types to align with Schema.

--- src/types/Item.ts
+++ src/types/Item.ts
@@ -3,40 +3,38 @@ import { Tag } from 'components/admin/tables/Tags';
 import { LeafletGeoJSON } from './Map';
 import { User } from './User';
 import { Ocean } from './Ocean';
-
-// enums as strings allows string comparisons. :)
-export enum Status {
-  PUBLISHED = 'Published',
-  UNPUBLISHED = 'Unpublished',
-  DRAFT = 'Draft'
-}
+import { License } from './License';
+import { S3Upload } from './s3Upload';
 
 export interface Item {
-  markerPosition: LeafletGeoJSON;
+  id: number,
 
-  sha512: string;
+  s3uploads_sha512: S3Upload;
   s3_key: string;
+
   created_at: string;
   updated_at: string;
   time_produced: string;
-  status: Status;
 
-  exif: {
-    [name: string]: string
-  };
+  status: boolean;
 
   concept_tags: Tag[];
   keyword_tags: Tag[];
-  recognition_tags: Tag[];
 
   place: string;
   country_or_ocean: string | Ocean;
+
+  item_type: number,
+
   creators: User[];
   contributor_login: string;
+
   directors: string[];
   writers: string[];
   collaborators: string;
+
   exhibited_at: string;
+
   series: string;
   ISBN: number;
   edition: number;
@@ -44,4 +42,12 @@ export interface Item {
   interviewers: string[];
   interviewees: string[];
   cast_: string;
+
+  license: License;
+
+  title: string;
+  description: string;
+
+  location: LeafletGeoJSON;
+
 }
---
+++ src/types/License.ts
@@ -0,0 +1,9 @@
+export enum License {
+  CC_BY= 'CC BY',
+  CC_BY_SA = 'CC BY-SA',
+  CC_BY_ND = 'CC BY-ND',
+  CC_BY_NC = 'CC BY-NC',
+  CC_BY_NC_SA = 'CC BY-NC-SA',
+  CC_BY_NC_ND = 'CC BY-NC-ND',
+  LOCKED = 'locked'
+}
