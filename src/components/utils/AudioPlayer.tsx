fcdc871d916dcf11aea68107d679ada8230dd3fe
Dan Wood <daniel@acrossthecloud.net>
2019-07-16 13:56:18 +1000

    WIP Audio Player

--- src/components/utils/AudioPlayer.tsx
+++ src/components/utils/AudioPlayer.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import WaveSurfer from 'wavesurfer.js';
 
 interface Props {
   file: string;
@@ -6,18 +7,56 @@ interface Props {
 
 interface State {
   file: string;
-  player: boolean;
-  location: string;
+  paused: boolean;
+  wavesurfer?: WaveSurfer;
 }
 
-class AudioPlayer extends React.Component<Props, State> {
+export class AudioPlayer extends React.Component<Props, State> {
+  _isMounted;
+
+  constructor(props: Props) {
+    super(props);
+
+    this._isMounted = false;
+
+    this.state = {
+      file: props.file,
+      paused: true,
+    };
+  }
+
+  componentDidMount(): void {
+    this._isMounted = true;
+
+    if (!this.state.wavesurfer) {
+      const
+        options = {
+          height: 80,
+          barHeight: 20,
+
+          container: '#wave',
+          backend: 'MediaElement',
+          responsive: true,
+
+          progressColor: '#4a74a5',
+          waveColor: '#cc0023',
+          cursorColor: '#4a74a5'
+        },
+        wavesurfer = WaveSurfer.create(options);
+
+      this.setState( { wavesurfer: wavesurfer } );
+
+      console.log(this.state.file, 'this.state.file');
+
+      wavesurfer.load(this.state.file);
+    }
+  }
+
   render() {
     return (
-      <audio>
-
-      </audio>
+      <>
+        <div id="wave"/>
+      </>
     );
   }
 }
-
-export default AudioPlayer;
