dae9e6b4915201d6384607b134674720ef8c86ec
Matthew Berryman <matthew@acrossthecloud.net>
2019-09-09 17:19:53 +1000

    try it this way

--- src/history.tsx
+++ src/history.tsx
@@ -1,3 +1,19 @@
 // history.js
 import { createBrowserHistory } from '../node_modules/history';
-export default createBrowserHistory();
+import ReactGA from 'react-ga';
+
+import config from './config';
+
+ReactGA.initialize(config.google.UA_ID);
+
+const history = createBrowserHistory();
+
+const historyListener = (location) => {
+  ReactGA.set({ page: location.pathname });
+	ReactGA.pageview(location.pathname);
+}
+
+history.listen(historyListener);
+historyListener(window.location);
+
+export default history;
