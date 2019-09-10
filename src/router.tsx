26a3b93691723c59a77739ca67efdcab4f9ab44e
Dan Wood <daniel@acrossthecloud.net>
2019-09-10 15:05:14 +1000

    BG Fix

--- src/router.tsx
+++ src/router.tsx
@@ -105,41 +105,38 @@ export const AppRouter = () => {
             />
 
             <Route exact path="/" component={Home} />
-            <div id="content">
-              <Route exact path="/view" component={ViewItems} />
-              <Route
-                path="/view/:itemId"
-                render={() => (
-                  <div className="container-fluid">
-                    <ViewItem />
-                  </div> )
+            <Route exact path="/view" component={ViewItems} />
+            <Route
+              path="/view/:itemId"
+              render={() => (
+                <div className="container-fluid content blue">
+                  <ViewItem />
+                </div> )
+              }
+            />
+            <Route exact path="/map" component={MapView} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/signup" component={SignUp} />
+            <Route exact path="/resetPassword/" component={ResetPassword} />
+            <Route exact path="/viewGraph" component={NetworkGraph} />
+
+            <Route exact path="/confirm/:email" component={AccountConfirmation} />
+
+            <AuthConsumer>
+              {({isLoading, authorisation, isAuthenticated}) => {
+                if (!isLoading) {
+                  return (
+                    <div className="main">
+                      <AdminRoutes authorisation={authorisation} history={history} />
+                      <CollaboratorRoutes authorisation={authorisation} history={history} />
+                      <LoggedInRoutes isAuthenticated={isAuthenticated} history={history} />
+                    </div>
+                  );
+                } else {
+                  return <></>;
                 }
-              />
-              <Route exact path="/map" component={MapView} />
-              <Route exact path="/login" component={Login} />
-              <Route exact path="/signup" component={SignUp} />
-              <Route exact path="/resetPassword/" component={ResetPassword} />
-              <Route exact path="/viewGraph" component={NetworkGraph} />
-
-              <Route exact path="/confirm/:email" component={AccountConfirmation} />
-
-              <AuthConsumer>
-                {({isLoading, authorisation, isAuthenticated}) => {
-                  if (!isLoading) {
-                    return (
-                      <>
-                        <AdminRoutes authorisation={authorisation} history={history} />
-                        <CollaboratorRoutes authorisation={authorisation} history={history} />
-                        <LoggedInRoutes isAuthenticated={isAuthenticated} history={history} />
-                      </>
-                    );
-                  } else {
-                    return <></>;
-                  }
-                }}
-              </AuthConsumer>
-            </div>
-
+              }}
+            </AuthConsumer>
           </div>
         </AuthProvider>
       </Router>
