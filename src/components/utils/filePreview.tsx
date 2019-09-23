285327da47b1ea431b14a2fa532903d23ae0617b
Dan Wood <daniel@acrossthecloud.net>
2019-09-24 08:51:18 +1000

    OC-302 Removed background blurred image behind Images and Videos (the poster)

--- src/components/utils/filePreview.tsx
+++ src/components/utils/filePreview.tsx
@@ -7,10 +7,10 @@ import { thumbnailsSRCSET } from './s3File';
 export const FilePreview = (props: { file: S3File }): JSX.Element => {
   switch (props.file.type) {
     case FileTypes.Image:
-      let background: string | undefined = undefined;
-      if (props.file.thumbnails) {
-        background = props.file.thumbnails['1140'] || props.file.thumbnails['960'] || props.file.thumbnails['720'] || props.file.thumbnails['540'];
-      }
+      // let background: string | undefined = undefined;
+      // if (props.file.thumbnails) {
+      //   background = props.file.thumbnails['1140'] || props.file.thumbnails['960'] || props.file.thumbnails['720'] || props.file.thumbnails['540'];
+      // }
       return (
         <Col className="px-0 image text-center">
           <img
@@ -18,11 +18,11 @@ export const FilePreview = (props: { file: S3File }): JSX.Element => {
             src={props.file.url}
             alt=""
           />
-          <div className="background" style={{ background: `url(${!!background ? encodeURI(background) : props.file.url})`, backgroundSize: 'contain' }} />
+          {/*<div className="background" style={{ background: `url(${!!background ? encodeURI(background) : props.file.url})`, backgroundSize: 'contain' }} />*/}
         </Col>
       );
     case FileTypes.Video:
-      const poster = !!props.file.poster ? props.file.poster : '';
+      // const poster = !!props.file.poster ? props.file.poster : '';
 
       return (
         <Col className="px-0 h-100 video">
@@ -32,7 +32,7 @@ export const FilePreview = (props: { file: S3File }): JSX.Element => {
             vertical-align="top"
             className="player"
           />
-          {!!poster ? <div className="background" style={{background: `url(${poster})`, backgroundSize: 'contain'}} /> : <></>}
+          {/*{!!poster ? <div className="background" style={{background: `url(${poster})`, backgroundSize: 'contain'}} /> : <></>}*/}
         </Col>
       );
     case FileTypes.Pdf:
