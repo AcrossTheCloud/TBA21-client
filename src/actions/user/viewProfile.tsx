c637ce9fb02220687866737fcb8c8d37ffcf3843
Qiao Lin <qiaoclin@gmail.com>
2019-10-08 23:17:02 +0200

    basic styling

--- src/actions/user/viewProfile.tsx
+++ src/actions/user/viewProfile.tsx
@@ -1,51 +1,60 @@
-import { API } from 'aws-amplify';
+import { API } from "aws-amplify";
 import {
   FETCH_PROFILE,
   FETCH_PROFILE_ERROR,
   FETCH_PROFILE_ERROR_NO_SUCH_PROFILE
-} from '../../reducers/user/viewProfile';
-import { LOADINGOVERLAY } from '../loadingOverlay';
+} from "../../reducers/user/viewProfile";
+import { LOADINGOVERLAY } from "../loadingOverlay";
 /**
  *
  * API call to fetch profile information based on the profileID and dispatch it through to Redux
  *
  * @param profileId {string}
  */
-export const fetchProfile = (profileId: string) => async (dispatch, getState) => {
+export const fetchProfile = (profileId: string) => async (
+  dispatch,
+  getState
+) => {
   const prevState = getState();
   dispatch({ type: LOADINGOVERLAY, on: true }); // Turn on the loading overlay
 
   // Detect if we have the same profileID and return the previous state.
   // We do this here to stop another API call and you can easily get the prevState in the Action.
-  if (prevState.viewItem.profile && profileId === prevState.viewItem.profile.profileId) {
+  if (
+    prevState.viewItem.profile &&
+    profileId === prevState.viewItem.profile.profileId
+  ) {
     dispatch({ type: LOADINGOVERLAY, on: false }); // Turn off the loading overlay
     return prevState.viewItem;
   } else {
-
     try {
-      const response = await API.get('tba21', 'profiles', {
+      const response = await API.get("tba21", "profiles", {
         queryStringParameters: {
           id: profileId
         }
       });
 
-      if (!!response.profile && response.profile.length && response.profile[0]) {
+      if (
+        !!response.profile &&
+        response.profile.length &&
+        response.profile[0]
+      ) {
         const profile = response.profile[0];
 
         dispatch({
-           type: FETCH_PROFILE,
-           profile: profile
-         });
+          type: FETCH_PROFILE,
+          profile: profile
+        });
       } else {
         dispatch({
-           type: FETCH_PROFILE_ERROR_NO_SUCH_PROFILE,
-           profile: {}
-         });
+          type: FETCH_PROFILE_ERROR_NO_SUCH_PROFILE,
+          profile: {}
+        });
       }
     } catch (e) {
       dispatch({
-         type: FETCH_PROFILE_ERROR
-       });
+        type: FETCH_PROFILE_ERROR
+      });
     } finally {
       dispatch({ type: LOADINGOVERLAY, on: false }); // Turn off the loading overlay
     }
--- src/components/user/profile/ViewProfile.tsx
+++ src/components/user/profile/ViewProfile.tsx
@@ -1,15 +1,15 @@
-import * as React from 'react';
-import { connect } from 'react-redux';
+import * as React from "react";
+import { connect } from "react-redux";
 
-import { State } from 'reducers/user/viewProfile';
+import { State } from "reducers/user/viewProfile";
 
-import { RouteComponentProps, withRouter } from 'react-router';
+import { RouteComponentProps, withRouter } from "react-router";
 
-import 'styles/components/pages/viewProfile.scss';
-import { Alerts, ErrorMessage } from '../../utils/alerts';
-import { Profile } from '../../../types/Profile';
-import { fetchProfile } from '../../../actions/user/viewProfile';
-import { Col, Row } from 'reactstrap';
+import "styles/components/pages/viewProfile.scss";
+import { Alerts, ErrorMessage } from "../../utils/alerts";
+import { Profile } from "../../../types/Profile";
+import { fetchProfile } from "../../../actions/user/viewProfile";
+import { Col, Row } from "reactstrap";
 
 interface Props extends RouteComponentProps, Alerts {
   fetchProfile: Function;
@@ -17,14 +17,15 @@ interface Props extends RouteComponentProps, Alerts {
 }
 
 class ViewProfile extends React.Component<Props, State> {
-  matchedId: string = '';
+  matchedId: string = "";
 
-  constructor(props: any) { // tslint:disable-line: no-any
+  constructor(props: any) {
+    // tslint:disable-line: no-any
     super(props);
 
     // Get our profileId passed through from URL props
     if (props.location && props.location.pathname) {
-      this.matchedId = props.location.pathname.replace('/profiles/', '');
+      this.matchedId = props.location.pathname.replace("/profiles/", "");
     }
   }
 
@@ -33,13 +34,13 @@ class ViewProfile extends React.Component<Props, State> {
     if (this.matchedId) {
       this.props.fetchProfile(this.matchedId);
     } else {
-      this.setState({ errorMessage: 'No profile with that id.' });
+      this.setState({ errorMessage: "No profile with that id." });
     }
   }
 
   render() {
-    if (typeof this.props.profile === 'undefined') {
-      return 'Loading...';
+    if (typeof this.props.profile === "undefined") {
+      return "Loading...";
     }
     const {
       full_name,
@@ -50,50 +51,69 @@ class ViewProfile extends React.Component<Props, State> {
       biography,
       website,
       field_expertise,
-      profile_type,
+      profile_type
       // public_profile
     } = this.props.profile;
-    return (
-      <div id="profile" >
-        <ErrorMessage message={this.props.errorMessage} />
-         <Row>
-          <Col xs="12" md="6" className="left">
-              <Col xs="12">
-                {
-                  !!this.props.profile.profile_image ?
-                    <img src={this.props.profile.profile_image}  alt="" />
-                    : <></>
-                }
-              </Col>
-              <Col xs="12" >
-                <h1>{full_name}</h1>
-              </Col>
-              <Col xs="12">
-                {field_expertise}
-              </Col>
-              <Col xs="12">
-                {profile_type}
-              </Col>
-              <Col xs="12">
-                {biography}
-              </Col>
-          </Col>
+    const tags = ["marine wildlife", "adaptations at sea", "climate change"];
 
-          <Col xs="12" md="6" className="right">
-            <Row className="detailsRow">
-              <Col xs="12" className="details border">
-                Location: {city}, {country}
+    return (
+      <div id="profile">
+        <ErrorMessage message={this.props.errorMessage} />
+        <Row>
+          <Col xs="12" md="6" className="left">
+            <Row className="m-3">
+              <Col xs="12" md="4">
+                {!!this.props.profile.profile_image ? (
+                  <img
+                    className="responsive-img"
+                    src={this.props.profile.profile_image}
+                    alt=""
+                  />
+                ) : (
+                  <></>
+                )}
               </Col>
-              <Col xs="12" className="details border">
-                Website: {website}
-              </Col>
-              <Col xs="12" className="details border">
-                Position: {position}
-              </Col>
-              <Col xs="12" className="details border">
-                Affiliation: {affiliation}
+              <Col xs="12" md="8" className="biography-section">
+                <h1>{full_name}</h1>
+                <div className="align-bottom profession">
+                  <div> {field_expertise} </div>
+                  <div className="caption-text">{profile_type} Contributor</div>
+                </div>
               </Col>
             </Row>
+            <Col xs="12">{biography}</Col>
+          </Col>
+          <Col xs="12" md="6">
+            <div className="m-3 details caption-text">
+              <div>
+                Location:
+                <span className="ml-auto">
+                  {city}, {country}
+                </span>
+              </div>
+              <div>
+                Website:
+                <span className="ml-auto">
+                  <a href={website || ""} target="_blank">
+                    {website}
+                  </a>
+                </span>
+              </div>
+              <div>
+                Position: <span className="ml-auto">{position}</span>
+              </div>
+              <div>
+                Affiliation: <span className="ml-auto">{affiliation}</span>
+              </div>
+              <div>
+                Most used concept tags
+                <p className="mt-5">
+                  {tags.map(ea => (
+                    <span className="tags">{ea}</span>
+                  ))}
+                </p>
+              </div>
+            </div>
           </Col>
         </Row>
       </div>
@@ -102,7 +122,8 @@ class ViewProfile extends React.Component<Props, State> {
 }
 
 // State to props
-const mapStateToProps = (state: { viewProfile: State }) => { // tslint:disable-line: no-any
+const mapStateToProps = (state: { viewProfile: State }) => {
+  // tslint:disable-line: no-any
   return {
     errorMessage: state.viewProfile.errorMessage,
     profile: state.viewProfile.profile
@@ -110,4 +131,9 @@ const mapStateToProps = (state: { viewProfile: State }) => { // tslint:disable-l
 };
 
 // Connect our redux store State to Props, and pass through the fetchProfile function.
-export default withRouter(connect(mapStateToProps, { fetchProfile })(ViewProfile));
+export default withRouter(
+  connect(
+    mapStateToProps,
+    { fetchProfile }
+  )(ViewProfile)
+);
