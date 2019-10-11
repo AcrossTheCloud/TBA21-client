be9d55168f8b1472936b6ecf82a523c325cf0d8b
Dan Wood <daniel@acrossthecloud.net>
2019-10-11 15:46:13 +1100

    Missed these ones.

--- src/REST/items.ts
+++ src/REST/items.ts
@@ -2,11 +2,11 @@ import { API } from 'aws-amplify';
 import { TopoJSON } from 'topojson-specification';
 
 export const getItems = async (queryStringParameters: Object = {}): Promise<TopoJSON> => {
-  const response = await API.get('tba21', `items/get`, { queryStringParameters: queryStringParameters });
+  const response = await API.get('tba21', `items/get`, { queryStringParameters });
   return response.data;
 };
 export const getItem = async (queryStringParameters: Object = {}): Promise<TopoJSON> => {
-  const response = await API.get('tba21', `items/getItem`, { queryStringParameters: queryStringParameters });
+  const response = await API.get('tba21', `items/getItem`, { queryStringParameters });
   return response.data;
 };
 
@@ -14,11 +14,19 @@ export const getItem = async (queryStringParameters: Object = {}): Promise<TopoJ
  * ADMIN FUNCTIONS
  */
 export const adminGetItems = async (queryStringParameters: Object = {}): Promise<TopoJSON> => {
-  const response = await API.get('tba21', `admin/items`, { queryStringParameters: queryStringParameters });
+  const response = await API.get('tba21', `admin/items`, { queryStringParameters });
   return response.data;
 };
 
 export const adminGetItem = async (isContributorPath: boolean = false, queryStringParameters: {s3Key: string}): Promise<TopoJSON> => {
+  const response = await API.get('tba21', (isContributorPath ? 'contributor/items/getItem' : 'admin/items/getItemNC'), { queryStringParameters });
+  return response.data;
+};
+
+/*
+ * CONTRIBUTOR FUNCTIONS
+ */
+export const contributorGetByPerson = async (isContributorPath: boolean = false, queryStringParameters: Object = {}): Promise<TopoJSON> => {
   const response = await API.get('tba21', (isContributorPath ? 'contributor/items/getItem' : 'admin/items/getItemNC'), { queryStringParameters });
   return response.data;
 };
