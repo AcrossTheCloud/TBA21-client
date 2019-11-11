92d5be3591b357a921c292e70e08a002cf1fa02a
Dan Wood <daniel@acrossthecloud.net>
2019-11-12 10:38:32 +1100

    OC-357 Ocean map layer.

--- src/components/map/utils/initialiseMap.ts
+++ src/components/map/utils/initialiseMap.ts
@@ -8,20 +8,17 @@ import * as L from 'leaflet';
  */
 export function initialiseMap(element: string = 'oa_map', viewPosition?: L.LatLngTuple): L.Map {
   const
-    mapID: string = 'mapbox.outdoors',
+    mapID: string = 'ck2v1bjni03r81cpan7p75w4v',
     accessToken: string = 'pk.eyJ1IjoiYWNyb3NzdGhlY2xvdWQiLCJhIjoiY2ppNnQzNG9nMDRiMDNscDh6Zm1mb3dzNyJ9.nFFwx_YtN04_zs-8uvZKZQ',
-    tileLayerURL: string = 'https://api.tiles.mapbox.com/v4/' + mapID + '/{z}/{x}/{y}.png?access_token=' + accessToken;
+    tileLayerURL: string = 'https://api.mapbox.com/styles/v1/acrossthecloud/' + mapID + '/tiles/256/{z}/{x}/{y}@2x?access_token=' + accessToken;
 
   const map = L.map(element, {
+    minZoom: 3,
     maxZoom: 18,
     zoom: 5,
     center: [0, 0],
     preferCanvas: true
   });
-
-  if (viewPosition) {
-   map.setView([viewPosition[0], viewPosition[1]], 5);
-  }
 
   L.tileLayer(tileLayerURL, {
     attribution: '',
@@ -29,6 +26,10 @@ export function initialiseMap(element: string = 'oa_map', viewPosition?: L.LatLn
     id: mapID,
     accessToken: accessToken
   }).addTo(map);
+
+  if (viewPosition) {
+   map.setView([viewPosition[0], viewPosition[1]], 5);
+  }
 
   return map;
 }
