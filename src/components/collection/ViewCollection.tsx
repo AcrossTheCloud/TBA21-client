6cf6a2ed21364e026f40a9f7070266ea8ce5f980
Matt Rowles <matthewrowles@gmail.com>
2020-03-09 13:48:57 +1100

    Hoists only first item of the collection to the top and moves the collection view back to the bottom of the view Tidies up response logic and fixes bug where props.data might not be ready for rendering yet
    OC-376

--- src/components/collection/ViewCollection.tsx
+++ src/components/collection/ViewCollection.tsx
@@ -59,69 +59,79 @@ const CollectionDetails = (props: { label: string, value: string }): JSX.Element
 const DataLayout = (props: { data: Item | Collection, itemModalToggle?: Function, collectionModalToggle?: Function }): JSX.Element => {
   let response: JSX.Element = <></>;
 
-  if (props.data.__typename === 'item') {
-    const data = props.data as Item;
-    if (data.item_type === itemType.Audio || data.file.type === FileTypes.Audio) {
-      const date = dateFromTimeYearProduced(data.time_produced, data.year_produced);
-      response = (
-        <AudioPreview
-          data={{
-            id: data.id,
-            url: data.file.url,
-            title: data.title ? data.title : '',
-            creators: data.creators ? data.creators : undefined,
-            item_subtype: data.item_subtype ? data.item_subtype : undefined,
-            date: date,
-            isCollection: data.__typename !== 'item'
-          }}
-          noClick={data.__typename !== 'item'}
-        />
-      );
-    } else {
-      response = <DetailPreview data={data} modalToggle={typeof props.itemModalToggle === 'function' ? props.itemModalToggle : undefined}/>;
-    }
-  } else {
-    const data = props.data as Collection;
-    if (data.file && data.id) {
-      getCollectionsInCollection({id: data.id, limit: 1000, offset: 0}).then(collectionResponse => {
-        const collections = [...removeTopology(collectionResponse, 'collection')] as Collection[];
-        data.collections = collections.map((collectionItem) => collectionItem.id) as string[];
-      });
-      getItemsInCollection({id: data.id, limit: 1000, offset: 0}).then(itemResponses => {
-        const items = [...removeTopology(itemResponses, 'item')] as Item[];
-        data.items = items.map((itemItem) => itemItem.id) as string[];
-      });
+  if (props.data) {
+    if (props.data.__typename === 'item') {
+      const data = props.data as Item;
 
-      response = (
-        <DetailPreview
-          modalToggle={() => typeof props.collectionModalToggle === 'function' ? props.collectionModalToggle(data) : undefined}
-          data={{
-            file: data.file,
-            id: data.id,
-            title: data.title ? data.title : '',
-            s3_key: '',
-            year_produced: data.year_produced ? data.year_produced : '',
-            time_produced: data.time_produced ? data.time_produced : '',
-            creators: data.creators ? data.creators : [],
-            regions: data.regions ? data.regions : [],
-            created_at: data.created_at ? data.created_at : null,
-            items: data.items as any || [],
-            collections: data.collections as any || [],
+      if (data.item_type === itemType.Audio || data.file.type === FileTypes.Audio) {
+        const date = dateFromTimeYearProduced(data.time_produced, data.year_produced);
+        response = (
+            <AudioPreview
+                data={{
+                  id: data.id,
+                  url: data.file.url,
+                  title: data.title ? data.title : '',
+                  creators: data.creators ? data.creators : undefined,
+                  item_subtype: data.item_subtype ? data.item_subtype : undefined,
+                  date: date,
+                  isCollection: data.__typename !== 'item'
+                }}
+                noClick={data.__typename !== 'item'}
+            />
+        );
+      } else {
+        response = (
+            <DetailPreview
+              data={data}
+              modalToggle={typeof props.itemModalToggle === 'function' ? props.itemModalToggle : undefined}
+            />
+        );
+      }
+    } else if (props.data.__typename === 'collection') {
+      const data = props.data as Collection;
 
-            // Collection specific
-            count: data.count ? data.count : 0,
-            type: data.type ? data.type : undefined,
+      if (data.file && data.id) {
+        getCollectionsInCollection({id: data.id, limit: 1000, offset: 0}).then(collectionResponse => {
+          const collections = [...removeTopology(collectionResponse, 'collection')] as Collection[];
+          data.collections = collections.map((collectionItem) => collectionItem.id) as string[];
+        });
+        getItemsInCollection({id: data.id, limit: 1000, offset: 0}).then(itemResponses => {
+          const items = [...removeTopology(itemResponses, 'item')] as Item[];
+          data.items = items.map((itemItem) => itemItem.id) as string[];
+        });
 
-            concept_tags: [],
-            keyword_tags: []
-          }}
-        />
-      );
+        response = (
+            <DetailPreview
+                modalToggle={() => typeof props.collectionModalToggle === 'function' ? props.collectionModalToggle(data) : undefined}
+                data={{
+                  file: data.file,
+                  id: data.id,
+                  title: data.title ? data.title : '',
+                  s3_key: '',
+                  year_produced: data.year_produced ? data.year_produced : '',
+                  time_produced: data.time_produced ? data.time_produced : '',
+                  creators: data.creators ? data.creators : [],
+                  regions: data.regions ? data.regions : [],
+                  created_at: data.created_at ? data.created_at : null,
+                  items: data.items as any || [],
+                  collections: data.collections as any || [],
+                  // Collection specific
+                  count: data.count ? data.count : 0,
+                  type: data.type ? data.type : undefined,
+                  concept_tags: [],
+                  keyword_tags: []
+                }}
+            />
+        );
+      }
     }
   }
 
   return (
-    <Col md={!!props.data.file && props.data.file.type === 'Audio' ? '8' : '4'} className="pt-4">
+    <Col
+        md={!!props.data && !!props.data.file && props.data.file.type === 'Audio' ? '8' : '4'}
+        className="pt-4"
+    >
       {response}
     </Col>
   );
@@ -328,10 +338,21 @@ class ViewCollection extends React.Component<Props, State> {
       <div id="item" className="container-fluid">
         <ErrorMessage message={this.props.errorMessage} />
 
-        <Row id={this.state.dataRowID}>
+        <Row>
           {
             this.state.data ?
-                this.state.data.map((data: Item | Collection, i) => <DataLayout data={data} key={i} itemModalToggle={this.props.itemModalToggle} collectionModalToggle={this.collectionModalToggle}/>)
+                [this.state.data
+                    .filter((data: Item | Collection, i) => {
+                      return data.__typename === 'item';
+                    })[0]]
+                    .map((data: Item | Collection, i) => (
+                        <DataLayout
+                            data={data}
+                            key={i}
+                            itemModalToggle={this.props.itemModalToggle}
+                            collectionModalToggle={this.collectionModalToggle}
+                        />
+                    ))
                 : <></>
           }
         </Row>
@@ -380,6 +401,14 @@ class ViewCollection extends React.Component<Props, State> {
               )
               : <></>
             }
+
+            <Row id={this.state.dataRowID}>
+              {
+                this.state.data ?
+                    this.state.data.map((data: Item | Collection, i) => <DataLayout data={data} key={i} itemModalToggle={this.props.itemModalToggle} collectionModalToggle={this.collectionModalToggle}/>)
+                    : <></>
+              }
+            </Row>
           </Col>
 
           <Col xs="12" md="4" className="right">
@@ -463,4 +492,4 @@ const mapStateToProps = (state: { viewCollection: ViewCollectionState }, props: 
 };
 
 // Connect our redux store State to Props, and pass through the fetchCollection function.
-export default withRouter(connect(mapStateToProps, { fetchCollection, dispatchLoadMore, itemModalToggle })(ViewCollection))
+export default withRouter(connect(mapStateToProps, { fetchCollection, dispatchLoadMore, itemModalToggle })(ViewCollection));
