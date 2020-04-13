d30c4186d79707eee2fb1979dc3d28c78751eb90
Matthew Berryman <matthew@acrossthecloud.net>
2020-04-14 08:28:54 +1000

    change name of licence

--- src/types/License.ts
+++ src/types/License.ts
@@ -5,5 +5,5 @@ export enum License {
   CC_BY_NC = 'CC BY-NC',
   CC_BY_NC_SA = 'CC BY-NC-SA',
   CC_BY_NC_ND = 'CC BY-NC-ND',
-  LOCKED = 'Ocean Archive'
+  LOCKED = 'Ocean Archive Restrictive License'
 }
