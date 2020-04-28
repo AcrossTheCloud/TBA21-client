c4126d36d14612ccb0c8dd411b03421e30ba4823
Matthew Berryman <matthew@acrossthecloud.net>
2020-04-28 11:17:27 +1000

    move boolean sliders up on collection editor

--- src/components/metadata/CollectionEditor.tsx
+++ src/components/metadata/CollectionEditor.tsx
@@ -1491,6 +1491,17 @@ class CollectionEditorClass extends React.Component<Props, State> {
                         onChange={e => this.changeCollection('subtitle', e.target.value)}
                       />
                     </FormGroup>
+                    <div>
+                      <InputGroup>
+                        <CustomInput type="switch" id={`${this.state.collection.id}_oa_highlight`} name="OA_highlight" label="OA Highlight" checked={!!this.state.collection.oa_highlight || false} onChange={e => this.changeCollection('oa_highlight', e.target.checked)} />
+                      </InputGroup>
+                      <InputGroup>
+                        <CustomInput type="switch" id={`${this.state.collection.id}_oa_original`} name="OA_original" label="OA Original" checked={!!this.state.collection.oa_original || false} onChange={e => this.changeCollection('oa_original', e.target.checked)} />
+                      </InputGroup>
+                      <InputGroup>
+                        <CustomInput type="switch" id={`${this.state.collection.id}_tba21_material`} name="TBA21_material" label="TBA21 Material" checked={!!this.state.collection.tba21_material || false} onChange={e => this.changeCollection('tba21_material', e.target.checked)} />
+                      </InputGroup>
+                    </div>
                     <FormGroup>
                       <Label for="description">Description</Label>
                       <RichTextEditor
@@ -1661,18 +1672,6 @@ class CollectionEditorClass extends React.Component<Props, State> {
                           <CustomInput type="checkbox" id={`${id}_focus_action`} defaultChecked={(typeof focus_action !== 'undefined' && focus_action !== null) && parseInt(focus_action, 0) > 0} onChange={e => this.changeCollection('focus_action', e.target.checked ? '1' : '0')}/>
                         </Col>
                       </FormGroup>
-                    </div>
-
-                    <div>
-                      <InputGroup>
-                        <CustomInput type="switch" id={`${this.state.collection.id}_oa_highlight`} name="OA_highlight" label="OA Highlight" checked={!!this.state.collection.oa_highlight || false} onChange={e => this.changeCollection('oa_highlight', e.target.checked)} />
-                      </InputGroup>
-                      <InputGroup>
-                        <CustomInput type="switch" id={`${this.state.collection.id}_oa_original`} name="OA_original" label="OA Original" checked={!!this.state.collection.oa_original || false} onChange={e => this.changeCollection('oa_original', e.target.checked)} />
-                      </InputGroup>
-                      <InputGroup>
-                        <CustomInput type="switch" id={`${this.state.collection.id}_tba21_material`} name="TBA21_material" label="TBA21 Material" checked={!!this.state.collection.tba21_material || false} onChange={e => this.changeCollection('tba21_material', e.target.checked)} />
-                      </InputGroup>
                     </div>
 
                   </Col>
