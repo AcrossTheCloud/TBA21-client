8512b52aa83694ee2c7e20577ec0d270c467f2f8
Matthew Berryman <matthew@acrossthecloud.net>
2020-04-29 13:12:38 +1000

    pop this back in place

--- src/prod-config.js
+++ src/prod-config.js
@@ -6,7 +6,7 @@ export default {
   },
   apiGateway: {
     REGION: "eu-central-1",
-    URL: "http://localhost:8080/"
+    URL: "https://demo-api.ocean-archive.org/"
   },
   cognito: {
     REGION: "eu-central-1",
