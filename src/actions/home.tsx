8a488ccc871a0b0cfc73d9b39fc235e900758bf6
Matthew Berryman <matthew@acrossthecloud.net>
2020-04-29 15:00:11 +1000

    collection type tidy up in oa_highlights

--- src/actions/home.tsx
+++ src/actions/home.tsx
@@ -130,9 +130,11 @@ export const loadHomepage = () => async dispatch => {
             {data.title.length > 45 ? data.title.substr(0, 44) + '...' : data.title}
           </div>
         </div>
-        <div className="type mb-2" onClick={() => dispatch(openModal(data))}>
-          {data.item_subtype ? data.item_subtype : data.type}{data.time_produced || data.year_produced ? ', ' : '' }{dateFromTimeYearProduced(data.time_produced, data.year_produced, data.end_year_produced)}
-        </div>
+        { data.item_type ? 
+          <div className="type mb-2" onClick={() => dispatch(openModal(data))}>
+            {data.item_subtype ? data.item_subtype : data.type}{data.time_produced || data.year_produced ? ', ' : '' }{dateFromTimeYearProduced(data.time_produced, data.year_produced, data.end_year_produced)}
+          </div> : <></>
+        }
         {!!tags && tags.length ?
           <div className="tagWrapper tags d-none d-lg-block">
             {
