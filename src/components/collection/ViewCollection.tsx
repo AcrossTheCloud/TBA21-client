6bf356e951a8ba05de7b68fc2ab76f808e91de58
Matthew Berryman <matthew@acrossthecloud.net>
2020-05-08 12:16:52 +1000

    WIP on making header not clickable also no details shown

--- src/components/collection/ViewCollection.tsx
+++ src/components/collection/ViewCollection.tsx
@@ -88,6 +88,7 @@ const DataLayout = (props: { data: Item | Collection, itemModalToggle?: Function
             <DetailPreview
               data={data}
               modalToggle={typeof props.itemModalToggle === 'function' ? props.itemModalToggle : undefined}
+              isBanner={true}
             />
         );
       }
