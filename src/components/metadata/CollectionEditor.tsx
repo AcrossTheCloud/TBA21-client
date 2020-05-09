a99040c11b6fa75232e70167dc44c2e81a3f5aca
Matthew Berryman <matthew@acrossthecloud.net>
2020-05-09 10:04:37 +1000

    exhibition collection changes

--- src/components/metadata/CollectionEditor.tsx
+++ src/components/metadata/CollectionEditor.tsx
@@ -484,6 +484,8 @@ class CollectionEditorClass extends React.Component<Props, State> {
       expedition_leader,
       expedition_route,
       city_of_publication,
+      curator,
+      related_material,
       media_type,
     } = this.state.collection;
 
@@ -516,7 +518,10 @@ class CollectionEditorClass extends React.Component<Props, State> {
         },
         'Exhibition' : {
           'institution': (institution || false),
-          'start_date': (start_date || false)
+          'start_date': (start_date || false),
+          'end_date': (end_date || false),
+          'curator': (curator || false),
+          'related_material': (related_material || false)
         },
         'Collection' : {
           'institution': (institution || false),
