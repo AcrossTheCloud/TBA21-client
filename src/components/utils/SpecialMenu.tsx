0251fb05fa6040325f63f91df60262a54d7cc71d
Matthew Berryman <matthew@acrossthecloud.net>
2020-05-19 21:18:12 +1000

    special menu changes

--- src/components/utils/SpecialMenu.tsx
+++ src/components/utils/SpecialMenu.tsx
@@ -12,30 +12,30 @@ export default class SpecialMenu extends Component<Props,{}> {
   mappings = {
     '893': {title: 'Multiple Oceans'},
     '894': {title: 'Sea Level Rise'},
-    '902': {title: 'Sensible Zone'},
-    '895': {title: 'Atmosphere'},
-    '904': {title: 'Eutrophication, Algae, Dead Zones'},
-    '899': {title: 'Overfishing'},
-    '901': {title: 'Transport / Shipment'},
-    '931': {title: 'Bathymetry / Extractivism'},
-    '900': {title: 'Continental Shelves'},
     '898': {title: 'Coastal Urbanization'},
     '897': {title: 'Coastal Transformation'},
-    '896': {title: 'The Knowledge of Land and Water: Indigenous Climate Justice'}
+    '902': {title: 'Sensible Zone'},
+    '904': {title: 'Chlorophyll and Algae Bloom / Dead Zones'},
+    '931': {title: 'Bathymetry'},
+    '900': {title: 'Continental Shelves'},
+    '899': {title: 'Overfishing'},
+    '901': {title: 'Transport / Shipment'},
+    '895': {title: 'Atmosphere'},
+    '896': {title: 'Climate Justice'}
   }
 
   mappings_ids_inOrder = [
     '893',
     '894',
-    '902',
-    '895',
-    '904',
-    '899',
-    '901',
-    '931',
-    '900',
     '898',
     '897',
+    '902',
+    '904',
+    '931',
+    '900',
+    '899',
+    '901',
+    '895',
     '896'
   ]
 
