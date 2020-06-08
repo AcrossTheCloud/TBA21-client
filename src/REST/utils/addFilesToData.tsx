4ccc84fc1999700c7bf606c3314fc53eb205fcf4
Philip Young <philip.young.gunawan@gmail.com>
2020-06-08 20:10:14 +0700

    wrap addfilestodata inside try catch

--- src/REST/utils/addFilesToData.tsx
+++ src/REST/utils/addFilesToData.tsx
@@ -6,48 +6,54 @@ import { FileTypes, S3File } from '../../types/s3File';
  * HEADS all files and inserts a file key value pair into the item/collection.
  * @param data
  */
-const addFilesToData = async(datas) => {
+const addFilesToData = async (datas) => {
   if (datas && datas.length) {
-    // Loop through each object in the array and get it's File from CloudFront
-    const files = await Promise.all(datas.map(d => getCDNObject(d.s3_key)))
+    try {
+      // Loop through each object in the array and get it's File from CloudFront
+      const files = await Promise.all(datas.map(d => getCDNObject(d.s3_key)))
 
-    let newDatas = files.map((f, i) => {
-      let data = datas[i];
-      let file = f as S3File
-      const s3Key = data.s3_key;
-      if (file.type === FileTypes.Image) {
-        const thumbnailUrl = `${config.other.THUMBNAIL_URL}${s3Key}`;
-        let thumbnails = {};
-        if (typeof data.file_dimensions !== 'undefined') {
-          const dimensions: number[] = data.file_dimensions as number[];
-          if (dimensions && dimensions[0]) {
-            if (dimensions[0] > 540) {
-              Object.assign(thumbnails, { 540: `${thumbnailUrl}.thumbnail540.png` });
-            }
-            if (dimensions[0] > 720) {
-              Object.assign(thumbnails, { 720: `${thumbnailUrl}.thumbnail720.png` });
-            }
-            if (dimensions[0] > 960) {
-              Object.assign(thumbnails, { 960: `${thumbnailUrl}.thumbnail960.png` });
-            }
-            if (dimensions[0] > 1140) {
-              Object.assign(thumbnails, { 1140: `${thumbnailUrl}.thumbnail1140.png` });
-            }
-            if (Object.keys(thumbnails).length > 1) {
-              Object.assign(file, { thumbnails });
+      let newDatas = files.map((f, i) => {
+        let data = datas[i];
+        let file = f as S3File
+        const s3Key = data.s3_key;
+        if (file.type === FileTypes.Image) {
+          const thumbnailUrl = `${config.other.THUMBNAIL_URL}${s3Key}`;
+          let thumbnails = {};
+          if (typeof data.file_dimensions !== 'undefined') {
+            const dimensions: number[] = data.file_dimensions as number[];
+            if (dimensions && dimensions[0]) {
+              if (dimensions[0] > 540) {
+                Object.assign(thumbnails, { 540: `${thumbnailUrl}.thumbnail540.png` });
+              }
+              if (dimensions[0] > 720) {
+                Object.assign(thumbnails, { 720: `${thumbnailUrl}.thumbnail720.png` });
+              }
+              if (dimensions[0] > 960) {
+                Object.assign(thumbnails, { 960: `${thumbnailUrl}.thumbnail960.png` });
+              }
+              if (dimensions[0] > 1140) {
+                Object.assign(thumbnails, { 1140: `${thumbnailUrl}.thumbnail1140.png` });
+              }
+              if (Object.keys(thumbnails).length > 1) {
+                Object.assign(file, { thumbnails });
+              }
             }
           }
         }
-      }
-      return {
-        ...data,
-        file: {
-          ...data.file,
-          ...file,
-        }
-      };
-    });
-    return newDatas;
+        return {
+          ...data,
+          file: {
+            ...data.file,
+            ...file,
+          }
+        };
+      });
+      return newDatas;
+    }
+    catch (e) {
+      throw e
+    }
+
   }
   else {
     return [];
